88e2e088b9bcc5fe893f6aa9d709dbcfd7ec34bc
Vera Reymkhe <reymkhe.v@gmail.com>
2018-10-23 14:56:27 +0600

    modified HelloWorld.java

--- HelloWorld/src/it/sevenbits/HelloWorld.java
+++ HelloWorld/src/it/sevenbits/HelloWorld.java
@@ -2,7 +2,7 @@ package it.sevenbits;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        // Prints "Hello World from branch1" to the terminal window.
+        System.out.println("Hello World from branch1");
     }
 }
