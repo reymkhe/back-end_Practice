62e8a1ff3015b9b83da3f5b47cdb680fe5ab86e6
Vera Reymkhe <reymkhe.v@gmail.com>
2018-12-11 17:35:54 +0600

    modified gitignore

---
+++ Practic2/src/main/java/com/ten_experts/summatra/array/exceptions/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package with exception for summater function
+ */
+package com.ten_experts.summatra.array.exceptions;
--- Practic2/src/main/java/com/ten_experts/summatra/array/package-info.java
+++ Practic2/src/main/java/com/ten_experts/summatra/array/package-info.java
@@ -1,4 +1,4 @@
 /**
- *
+ *Package with summater function
  */
 package com.ten_experts.summatra.array;
